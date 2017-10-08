4c1836fe1c7c7f30ba76411ec92e417b398e1c1a
MIKA <mika.mikangkim@gmail.com>
2017-10-07 22:06:29 -0400

    Created javascript in Survey page for sending summary

--- Comp229-Assign02/Comp229-Assign02/Pages/SurveyPage1.aspx.cs
+++ Comp229-Assign02/Comp229-Assign02/Pages/SurveyPage1.aspx.cs
@@ -14,6 +14,31 @@ namespace Comp229_Assign02.Pages
 
         }
 
+        protected void Submit_Click(object sender, EventArgs e)
+        {
+            string firstName = firstNameTextBox.Text.ToString();
+            Session["firstName"] = firstName;
+
+
+            string lastName = lastNameTextBox.Text.ToString();
+            Session["lastName"] = lastName;
+
+            string gender = genderRadio.SelectedValue;
+            Session["gender"] = gender;
+
+            string satisfaction = satisfactionRadio.SelectedValue;
+            Session["satisfaction"] = satisfaction;
+
+
+            string reason = reasonTextBox.Text.ToString();
+            Session["reason"] = reason;
+
+            string recommend = recommendDrop.SelectedValue;
+            Session["recommend"] = recommend;
+
+
+            Response.Redirect(Page.ResolveClientUrl("ThankPage.aspx"));
+        }
 
 
     }
